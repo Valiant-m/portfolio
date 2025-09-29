47752682105b979c37f98651128be2d4e2a38ea9
Valiant <miingvaliant@gmail.com>
2025-09-29 11:16:19 +0800

    Add google map address

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
-// =============================
-// NAVIGATION HANDLER
-// =============================
-
 const navLinks = document.querySelectorAll(".top-nav a");
 const sections = document.querySelectorAll(".content-section");
 
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
-    // Remove active class
+
     navLinks.forEach(l => l.classList.remove("active"));
     sections.forEach(sec => sec.classList.remove("active"));
 
-    // Add active to clicked
     link.classList.add("active");
     document.getElementById(link.dataset.section).classList.add("active");
   });
